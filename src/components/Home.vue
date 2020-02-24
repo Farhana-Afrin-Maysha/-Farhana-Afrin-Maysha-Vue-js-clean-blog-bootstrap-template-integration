b9f086baa830b15c0186c15540d92679ddabc65e
Farhana-Afrin-Maysha <farhanamaysha29@gmail.com>
2020-02-24 11:49:08 +0600

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>i am a paragraph</p>
+    </div>   
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  
+}
+</script>
+
+
+<style scoped>
+
+  color: #42b983;
+
+</style>
